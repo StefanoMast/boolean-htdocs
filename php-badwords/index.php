e564a4aad8b1c927c6ffdf0e701827910e3eacf6
StefanoMast <ste.mastrantonio@gmail.com>
2022-06-07 16:02:16 +0200

    first commit

---
+++ php-badwords/index.php
@@ -0,0 +1,13 @@
+<h1>
+<?php
+    $par = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ipsa in ab? Perferendis id doloremque, officiis cupiditate vero iusto, sed, quis voluptates suscipit delectus illo. Nobis quam ipsa distinctio nulla?";
+    ?>
+
+    <h2>Stampo paragrafo</h2>
+    <p>
+        <?php
+        echo $par;
+        ?>
+    </p>
+
+</h1>
